044d5eac2fcea72a17923c2ddcc6c8362967d186
petpetpeter <55285546+petpetpeter@users.noreply.github.com>
2021-10-15 11:06:29 +0900

    Update cloud_transformer.cpp

--- src/cloud_transformer.cpp
+++ src/cloud_transformer.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <ros/ros.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/point_cloud.h>
@@ -6,19 +7,32 @@
 #include <pcl/point_types.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <tf/transform_listener.h>
+#include <string>
+
 
 class CloudTransformer
 {
+
 public:
   explicit CloudTransformer(ros::NodeHandle nh)
     : nh_(nh)
   {
+    
     // Define Publishers and Subscribers here
     pcl_sub_ = nh_.subscribe("/inputPC", 1, &CloudTransformer::pclCallback, this);
     pcl_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/outputPC", 1);
 
+    // std::string frameIn;
+    // std::string frameOut;
+    // nh_.getParam("/cloud_tansformer/frameIn", frameIn);
+    // nh_.getParam("/cloud_tansformer/frameOut", frameOut); 
+    std::string frameIn;
+    std::string frameOut;
+    ros::param::get("/cloud_tansformer/frameIn", frameIn);
+    ros::param::get("/cloud_tansformer/frameOut", frameOut);
+    ROS_INFO("frameIn: %s, frameOut: %s", frameIn.c_str(), frameOut.c_str());
     buffer_.reset(new sensor_msgs::PointCloud2);
-    buffer_->header.frame_id = "/marker";
+    buffer_->header.frame_id = frameOut;
   }
 
 private:
@@ -27,12 +41,20 @@ private:
   ros::Publisher pcl_pub_;
   tf::TransformListener listener_;
   sensor_msgs::PointCloud2::Ptr buffer_;
+  
 
   void pclCallback(const sensor_msgs::PointCloud2ConstPtr& pcl_msg)
   {
-    listener_.waitForTransform("/marker", "/depth_camera_link", ros::Time::now(), ros::Duration(3.0));
-    pcl_ros::transformPointCloud("/marker", *pcl_msg, *buffer_, listener_);
+    
+    std::string frameIn;
+    std::string frameOut;
+    ros::param::get("/cloud_tansformer/frameIn", frameIn);
+    ros::param::get("/cloud_tansformer/frameOut", frameOut);
+    ROS_INFO("frameIn: %s, frameOut: %s", frameIn.c_str(), frameOut.c_str());
+    listener_.waitForTransform(frameOut, frameIn, ros::Time::now(), ros::Duration(3.0));
+    pcl_ros::transformPointCloud(frameOut, *pcl_msg, *buffer_, listener_);
     pcl_pub_.publish(buffer_);
+    
   }
 }; 
 
@@ -40,11 +62,13 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "point_cloud_tf");
   ros::NodeHandle nh;
+  
 
   CloudTransformer tranform_cloud(nh);
 
-  // ROS Loop
+  // Spin until ROS is shutdown
   while (ros::ok())
+    
     ros::spin();
 
   return 0;
